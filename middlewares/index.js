aebc00b06336fde9ab8c70aaf1e550fa1f6761fc
IngridSurya <41659390+IngridSurya@users.noreply.github.com>
2024-06-20 17:42:27 +0700

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,12 +1,10 @@
 const isLoggedIn = function (req, res, next) {
-    console.log(req.session);
-
     if (! req.session.userId) {
-        const error = 'Please login first!'
-        res.redirect(`/login?error=${error}`)
+        const error = 'Please login first!';
+        res.redirect(`/login?error=${error}`);
     } else {
         next();
     }
 }
 
-module.exports = isLoggedIn
+module.exports = isLoggedIn;
