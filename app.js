a2be25b76324745faf767c45fab7f37929452896
IngridSurya <41659390+IngridSurya@users.noreply.github.com>
2024-06-20 17:39:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const session = require('express-session')
+const session = require('express-session');
 const app = express();
 const port = 3000;
 
@@ -11,12 +11,12 @@ app.use(session({
     secure: false,
     sameSite: true
   }
-}))
+}));
 
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended: true}));
 app.use('/', require('./routers'));
 
 app.listen(port, () => {
-  console.log(`Web app listening on port ${port}`)
+  console.log(`Web app listening on port ${port}`);
 })
